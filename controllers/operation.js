3c309d699d94571309503660bbdd47af71d903ff
Girdhari <girdharichobdar@gmail.com>
2022-11-21 11:32:08 +0530

    add delete update endpoint error handler apis

--- controllers/operation.js
+++ controllers/operation.js
@@ -20,7 +20,6 @@ const createP =  (request, response) =>{
 
     phone_book.save().then(savedphone_book => {
         response.json(savedphone_book)
-        console.log(savedphone_book)
     })
 }
    
@@ -29,47 +28,46 @@ const getP = (req, res) =>{
         res.json(Phonebook)
     })
 }
-module.exports = {createP,getP}
 
-//   const deleteP =  async (req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const deletedphone_book = await phone_Book.findByIdAndDelete(id)
-//         res.send(`Document with ${deletedphone_book.contact_name} has been deleted..`)
-//     }
-//     catch (error) {
-//         res.status(400).json({ message: error.message })
-//     }
-// }
+
+  const deleteP =  async (req, res) => {
+    try {
+        const id = req.params.id;
+        const deletedphone_book = await Phonebook.findByIdAndDelete(id)
+        res.send(`Document with ${deletedphone_book.contact_name} has been deleted..`)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
   
-//   const updateP = async (req, res) => {
-//     try {
-//         const id = req.params.id;
-//         const updatedphone_book = req.body;
-//         const options = { new: true };
+  const updateP = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedphone_book = req.body;
+        const options = { new: true };
 
-//         const result = await phone_book.findByIdAndUpdate(
-//             id, updatedphone_book, options
-//         )
+        const result = await Phonebook.findByIdAndUpdate(
+            id, updatedphone_book, options
+        )
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+}
 
-//         res.send(result)
-//     }
-//     catch (error) {
-//         res.status(400).json({ message: error.message })
-//     }
-// }
 
-
-//   const getP_byid =  (request, response) => {
-//     phone_Book.findById(request.params.id).then(phone_Book => {
-//       response.json(phone_Book)
-//     })
-//   }
+  const getP_byid =  (req, res) => {
+    Phonebook.findById(req.params.id).then(Phonebook => {
+      res.json(Phonebook)
+    })
+  }
   
-//   const unknownEndpoint = (request, response) => {
-//     response.status(404).send({ error: 'unknown endpoint' })
-//   }
+  const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+  }
   
-//   app.use(unknownEndpoint)
+  
 
-
+module.exports = {createP,getP,deleteP,updateP,getP_byid,unknownEndpoint}
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const {createP,getP} = require('../controllers/operation');
+const {createP,getP,deleteP,updateP,getP_byid,unknownEndpoint} = require('../controllers/operation');
 const router = express.Router()
-//Post Method
+
 router.post('/operation', createP)
 router.get('/operation', getP)
+router.delete('/operation/:id', deleteP)
+router.patch('/operation/:id', updateP)
+router.get('/operation/:id', getP_byid)
+router.use('/operation', unknownEndpoint)
 
 module.exports = router;
 
